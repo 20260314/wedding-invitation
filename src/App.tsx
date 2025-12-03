82658809f30194f71cddb53d7785a106dbb88b4e
wedding260314 <jainy0v0@gmail.com>
2025-12-04 01:49:14 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ import { STATIC_ONLY } from "./env"
 
 function App() {
   return (
-    <div className="background">
-      <BGEffect />
       <div className="card-view">
         <LazyDiv className="card-group">
           {/* 표지 */}
