0fd4cefab660be8c26db1f17518646ca36bf930d
wedding260314 <jainy0v0@gmail.com>
2025-12-18 00:47:58 +0900

    Update index.tsx

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -1,12 +1,12 @@
 import { LazyDiv } from "../lazyDiv"
-import { COVER_IMAGE } from "../../images"
+import { ENDING_IMAGE } from "../../images"
 
 export const EndingImage = () => {
   return (
     <LazyDiv className="ending">
       <div className="image-wrapper">
         <img
-          src={COVER_IMAGE["ending1-2"]}
+          src={ENDING_IMAGE["ending1-2"]}
           alt="ending image"
         />
       </div>
